cce55485a93fa9824cfe6ee6bf953d92ef1c2522
Bogyoi <86239847+sonlucy@users.noreply.github.com>
2024-03-11 17:31:06 +0900

    로그아웃 기능 추가

--- src/component/AdminLogin.js
+++ src/component/AdminLogin.js
@@ -1,6 +1,5 @@
 
-
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import AdminPage from './Admin';
 import styled from 'styled-components';
 
@@ -29,8 +28,14 @@ const StyleContainer = styled.div`
   padding: 2rem;
   
 `
+
 const AdminLoginPage = () => {
   const [loggedIn, setLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const isLoggedIn = localStorage.getItem('isLoggedIn') === 'true';
+    setLoggedIn(isLoggedIn);
+  }, []);
 
   const handleLogin = () => {
     const adminId = prompt('아이디를 입력하세요:');
@@ -38,23 +43,30 @@ const AdminLoginPage = () => {
 
     // 'admin1' 아이디와 'password1' 비밀번호 일치하면 로그인 성공 (임시데이터)
     if (adminId === 'admin1' && adminPassword === 'password1') {
+      localStorage.setItem('isLoggedIn', true);
       setLoggedIn(true);
     } else {
       alert('아이디 또는 비밀번호가 올바르지 않습니다.');
     }
   };
 
+  const handleLogout = () => {
+    localStorage.removeItem('isLoggedIn');
+    setLoggedIn(false);
+  };
+
   return (
     <div>
       {!loggedIn ? (
         <StyleContainer>
-        <StyledMessage>관리자 페이지 접속을 위해선 관리자 아이디로 로그인 해야합니다.</StyledMessage>
-        <Button onClick={handleLogin}>관리자 로그인</Button>
+          <StyledMessage>관리자 페이지 접속을 위해선 관리자 아이디로 로그인 해야합니다.</StyledMessage>
+          <Button onClick={handleLogin}>관리자 로그인</Button>
         </StyleContainer>
       ) : (
         <div>
-            <StyledMessage>관리자 페이지에 로그인되었습니다.</StyledMessage>
-          <AdminPage/>
+          <StyledMessage>관리자 페이지에 로그인되었습니다.</StyledMessage>
+          <Button onClick={handleLogout}>로그아웃</Button>
+          <AdminPage />
         </div>
       )}
     </div>
