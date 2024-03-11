5bf20d6a812dbd35463802e77d56a4d7bd9e599f
Bogyoi <86239847+sonlucy@users.noreply.github.com>
2024-03-11 17:49:40 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,32 +1,37 @@
 import "./App.css";
 import Nav from "./component/navbar";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Qna from "./component/Qna";
 import UserInfoPage from "./component/Mypage";
 import QnaBoard from "./component/QnaBoard";
 import Footer from "./component/Footer";
 import Home from "./component/HomeContent";
+// import Login from "./component/Login";  
+
 import Title from "./component/Title";
-import Login from "./component/Login";
-// import SignUp from "./component/SignUp";
+import AdminPage from "./component/Admin";
+import MemberDetailQnaPage from "./component/MemberDetailQnaPage";
+import MemberDetailInfoPage from "./component/MemberDetailInfoPage";
+import AdminLoginPage from "./component/AdminLogin";
+
 
 function App() {
+  const isLoggedIn = false;
   return (
     <div>
       <BrowserRouter>
         <Nav />
-        <Title/> 
-        <Home />
-        <Repservice id="services"/>
-        <Solution id="solutions"/>
+        <Title />
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/qna" element={<Qna />}></Route>
           <Route path="/userinfo" element={<UserInfoPage />}></Route>
-          <Route path="/login" element={<Login />}></Route>
-          <Route path="/admin" element={<AdminPage />}></Route>
+
           <Route path="/admin/member/qna/:id" element={<MemberDetailQnaPage />}></Route>
           <Route path="/admin/member/userinfo/:id" element={<MemberDetailInfoPage />}></Route>
+
+          <Route path="/admin" element={isLoggedIn ? <Navigate to="/admin" /> : <AdminLoginPage />}></Route>
+          <Route path="/admin" element={isLoggedIn ? <AdminPage /> : <Navigate to="/admin" />}></Route>
         </Routes>
       </BrowserRouter>
       <Footer />
@@ -35,3 +40,4 @@ function App() {
 }
 
 export default App;
+
